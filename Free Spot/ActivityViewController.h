7501650dd8584318e9e7a147f63f56ce3e646ac8
ruizhu <ruizhu@me.com>
2014-02-10 01:36:33 -0500

    ruizhu 2/10 1:36am

--- Free Spot/ActivityViewController.h
+++ Free Spot/ActivityViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ActivityViewController : UIViewController
+@interface ActivityViewController : UITableViewController
+
 
 @end
--- Free Spot/ActivityViewController.m
+++ Free Spot/ActivityViewController.m
@@ -8,18 +8,39 @@
 
 #import "ActivityViewController.h"
 
-@interface ActivityViewController ()
+@interface ActivityViewController () {
+    NSDictionary *activityList;
+    NSArray *justActivityName;
+    NSMutableArray *_objects;
+}
+
+
 
 @end
 
 @implementation ActivityViewController
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return activityList.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"activityCell"];
+    cell.textLabel.text = justActivityName[indexPath.row];
+    cell.detailTextLabel.text = activityList[justActivityName[indexPath.row]];
+    return cell;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(insertNewObject:)];
+    self.navigationItem.rightBarButtonItem = addButton;
     
-    // This is a test change
+    NSURL *url = [[NSBundle mainBundle] URLForResource:@"Activity" withExtension:@".plist"];
+    activityList = [NSDictionary dictionaryWithContentsOfURL:url];
+    justActivityName = activityList.allKeys;
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -27,5 +48,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
